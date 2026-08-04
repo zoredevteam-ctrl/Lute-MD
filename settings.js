0547f646022b3e78eca57ca9baa11445e0a1eecb
˚₊· ͟͟͞͞  ɪ ᴀᴍ ᴋᴀᴍᴇᴋɪ <zoredevteam@gmail.com>
2026-08-04 13:24:46 -0500

    Crear settings.js

---
+++ settings.js
@@ -0,0 +1,23 @@
+// ─── LUTE MD · SETTINGS ──────────────────────────────────────────────────────
+
+global.botName    = 'Lute'
+global.botVersion = '1.0.0'
+global.botText    = 'Exterminadora · Hazbin Hotel'
+global.prefix     = '#'
+
+global.owner = [
+    ['573107400303', 'Arom', true],
+]
+
+global.prems = []
+
+global.sessionName = './sessions/owner'
+
+global.icon   = 'https://i.pinimg.com/736x/30/7e/3f/307e3f2df6f4a735f659c6f28a4fc399.jpg'
+global.banner = 'https://i.pinimg.com/736x/30/7e/3f/307e3f2df6f4a735f659c6f28a4fc399.jpg'
+
+global.newsletterJid  = '120363404822730259@newsletter'
+global.newsletterName = '𝐋𝐔𝐓𝐄'
+global.rcanal         = 'https://whatsapp.com/channel/0029Vb6p68rF6smrH4Jeay3Y'
+
+global.botOff = false
