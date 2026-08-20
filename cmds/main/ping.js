0d48171504ac3bb4b40bbfcbfafa7bfa9d0500a4
˚₊· ͟͟͞͞  ɪ ᴀᴍ ᴋᴀᴍᴇᴋɪ <zoredevteam@gmail.com>
2026-08-20 14:27:21 -0500

    Actualizar ping.js

--- cmds/main/ping.js
+++ cmds/main/ping.js
@@ -1,17 +1,14 @@
 const handler = async (m, { conn }) => {
+    const start = Date.now()
+
     const sent = await conn.sendMessage(m.chat, {
         text: '⚘ 𝘊𝘢𝘭𝘤𝘶𝘭𝘢𝘯𝘥𝘰... ⚘'
     }, { quoted: m })
 
-    const start = Date.now()
-
-    // Pequeña espera para medir la respuesta
-    await new Promise(resolve => setTimeout(resolve, 50))
-
     const ms = Date.now() - start
 
     await conn.sendMessage(m.chat, {
-        text: `𑁍 𝙋𝙞𝙣𝙜 𑁍\n> ✦ ${ms}ms`
+        text: `𖣔 𝙋𝙄𝙉𝙂 𖣔\n> ✦ ${ms}ms`
     }, {
         edit: sent.key
     })
